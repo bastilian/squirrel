6f18568aff5b8791e42e61abce5149009efb665d
bastilian <sebastian.graessl@gmail.com>
2009-05-16 12:26:50 +0200

    use a symbol to avoid deprecated-waring

--- test/schema.rb
+++ test/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(:version => 1) do
 		t.column :name, :string
 	end
 
-	create_table :posts_tags, id => false do |t|
+	create_table :posts_tags, :id => false do |t|
 		t.column :post_id, :integer
 		t.column :tag_id, :integer
 	end
